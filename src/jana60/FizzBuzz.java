416d80ecdabdeb538749adcddc47f21c7e8faba1
paskimeo <paskimeo@gmail.com>
2022-06-09 14:55:21 +0200

    inserito esercizio base

--- src/jana60/FizzBuzz.java
+++ src/jana60/FizzBuzz.java
@@ -3,8 +3,23 @@ package jana60;
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for (int i=1; i <= 100; i++) {
+			if (i %3 ==0 && i % 5 ==0) {
+				System.out.println("FITZ BUTZ");
+			}
+			if (i %3 == 0) {
+				System.out.println("FITZ");
+			
+			}
+			else if (i %5 == 0) {
+				System.out.println("BUTZ");
+			}
+			else {
+				System.out.println(i);
+			}
+		}
 
 	}
-
 }
+
+
