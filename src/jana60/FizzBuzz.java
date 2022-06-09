e914d14db18fe32313c8a13c610a9f800a95fd35
paskimeo <paskimeo@gmail.com>
2022-06-09 15:37:02 +0200

    inserito bonus

--- src/jana60/FizzBuzz.java
+++ src/jana60/FizzBuzz.java
@@ -1,9 +1,31 @@
 package jana60;
 
+import java.util.Scanner;
+
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		for (int i=1; i <= 100; i++) {
+		
+		
+		
+		Scanner scan = new Scanner(System.in);
+		boolean flag = false;
+		
+		while(flag==false) {
+			System.out.print("inserisci un un numero da 1 a 200:");
+			int num = scan.nextInt();
+		
+		
+			
+		
+		
+		
+		if (num >= 1 && num <= 200) {
+			flag = true;
+			
+		
+			
+		for (int i=1; i <= num; i++) {
 			if (i %3 ==0 && i % 5 ==0) {
 				System.out.println("FITZ BUTZ");
 			}
@@ -17,9 +39,10 @@ public class FizzBuzz {
 			else {
 				System.out.println(i);
 			}
+			
 		}
 
 	}
 }
-
-
+}
+}
